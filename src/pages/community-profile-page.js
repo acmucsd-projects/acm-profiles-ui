a2e6c8f865b372d0e8188808b959dca34b12092b
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-11-03 21:32:26 -0800

    Initialized page files for each page in the app with basic titles

---
+++ src/pages/community-profile-page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CommunityProfilePage() {
+  return (
+    <div>
+      <h1>Community Profile Page</h1>
+    </div>
+  );
+}
+
+export default CommunityProfilePage;

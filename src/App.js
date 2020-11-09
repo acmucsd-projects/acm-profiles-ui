e3cd0f689579e9ec3443bba4425e7c8cd90510a1
jettbui <jettcbui@gmail.com>
2020-11-08 20:09:52 -0800

    Added react-router package. Implemented basic navigation and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from "react"
-import logo from "./logo.svg"
+import { Switch, Route } from "react-router-dom"
+import CommunityProfilePage from "./pages/community-profile-page"
+import LogInPage from "./pages/log-in-page"
+import Navbar from "./components/navbar"
+import SearchPage from "./pages/search-page"
+import UserProfilePage from "./pages/user-profile-page"
 import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+
+      <Switch>
+        <Route path="/user">
+          <UserProfilePage />
+        </Route>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+        <Route path="/community">
+          <CommunityProfilePage />
+        </Route>
+        <Route path="/">
+          <LogInPage />
+        </Route>
+      </Switch>
     </div>
   )
 }

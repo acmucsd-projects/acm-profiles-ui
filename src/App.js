8750b87af9f6d5bd515531c62d13dc716bb60efa
abn <annie.nguyen.804@gmail.com>
2020-11-11 01:28:58 -0800

    Added navbar

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
+import CommunityProfilePage from './pages/community-profile-page';
+import SearchPage from './pages/search-page.js';
+import UserProfilePage from './pages/user-profile-page.js';
+import LogInPage from './pages/log-in-page.js';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/Navbar.js';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route path="/community-profile" component={CommunityProfilePage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/my-profile" component={UserProfilePage} />
+          <Route path="/login" component={LogInPage} />
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
 }
 
 export default App

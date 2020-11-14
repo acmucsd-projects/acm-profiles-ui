870841e5ee94d87dc6e88d60478bafdfa6a1c42e
abn <annie.nguyen.804@gmail.com>
2020-11-14 00:30:33 -0800

    Deleted extra import in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import App from "./App"
-import "jest-dom/extend-expect"
 
 test("renders LogInPage link", () => {
   const { getByText } = render(<App />)

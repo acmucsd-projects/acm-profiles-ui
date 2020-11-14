d4c55b160d4db67547738d05d906df3d5b90c730
abn <annie.nguyen.804@gmail.com>
2020-11-13 17:39:14 -0800

    Changed the test in App.test.js to pass linting

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import App from "./App"
+import "jest-dom/extend-expect"
 
-test("renders learn react link", () => {
+test("renders LogInPage link", () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+  const linkElement = getByText(/LogInPage/i)
   expect(linkElement).toBeInTheDocument()
 })

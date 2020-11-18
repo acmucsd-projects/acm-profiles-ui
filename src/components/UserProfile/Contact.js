dd67d4268a2a7bf70a4696077886b777dbe9f6f6
abn <annie.nguyen.804@gmail.com>
2020-11-17 22:38:44 -0800

    Deleted an import to fix dependency cycle

--- src/components/UserProfile/Contact.js
+++ src/components/UserProfile/Contact.js
@@ -1,9 +1,8 @@
 import React from "react"
-import ContactList from "./ContactList"
 import "./style.css"
 
 const Contact = () => {
-  return <div className="contact"><div /></div>
+  return <div className="contact" />
 }
 
 export default Contact

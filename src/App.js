640f86f55b5eaae69fa919e32602ded977958833
jettbui <jettcbui@gmail.com>
2020-11-18 13:17:25 -0800

    import from new NavBar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import CommunityProfilePage from "./pages/community-profile-page"
 import LogInPage from "./pages/log-in-page"
-import Navbar from "./components/Navbar"
+import NavigationBar from "./components/NavBar/NavBar"
 import SearchPage from "./pages/search-page"
 import UserProfilePage from "./pages/user-profile-page"
 import "./App.css"
@@ -13,15 +13,15 @@ function App() {
       <Router>
         <Switch>
           <Route path="/user">
-            <Navbar />
+            <NavigationBar />
             <UserProfilePage />
           </Route>
           <Route path="/search">
-            <Navbar />
+            <NavigationBar />
             <SearchPage />
           </Route>
           <Route path="/community">
-            <Navbar />
+            <NavigationBar />
             <CommunityProfilePage />
           </Route>
           {/* TODO: conditional routing if user is authenticated */}

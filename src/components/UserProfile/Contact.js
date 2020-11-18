22753df8694254207abd35f3de086341cd408687
Anurag Asthana <anurag.a.asthana@gmail.com>
2020-11-17 22:50:15 -0800

    Fixing linting errors

--- src/components/UserProfile/Contact.js
+++ src/components/UserProfile/Contact.js
@@ -2,15 +2,11 @@ import React from "react"
 import "./style.css"
 
 const Contact = () => {
-<<<<<<< HEAD
   return (
     <div className="contact">
       <div />
     </div>
   )
-=======
-  return <div className="contact" />
->>>>>>> dd67d4268a2a7bf70a4696077886b777dbe9f6f6
 }
 
 export default Contact

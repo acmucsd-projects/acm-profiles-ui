e9773b29a169e6d235d12a21427e54c7406dd75c
jettbui <jettcbui@gmail.com>
2020-11-18 13:17:49 -0800

    add placeholder navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,27 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const NavigationBar = () => {
   return (
     <div className="navigationbar">
-      <div />
+      {/* TODO: vvv Replace placeholder NavBar vvv */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Log In Page</Link>
+          </li>
+          <li>
+            <Link to="/community">Community Page</Link>
+          </li>
+          <li>
+            <Link to="/search">Search Page</Link>
+          </li>
+          <li>
+            <Link to="/user">User Page</Link>
+          </li>
+        </ul>
+      </nav>
+      {/* ^^^ */}
     </div>
   )
 }

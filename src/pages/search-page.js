4143fd62b2e8815bd42f53f862da15f864946948
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-11-19 17:13:37 -0800

    Created basic layout for search page, fixed log-in-page css class names

--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -1,9 +1,16 @@
 import React from "react"
+import "./search-page.css"
+import { Typography } from "antd"
+import "antd/dist/antd.css"
+
+const { Title } = Typography
 
 function SearchPage() {
   return (
-    <div>
-      <h1>Search Page</h1>
+    <div className="background">
+      <div className="page-body">
+        <Title size={1}>Search</Title>
+      </div>
     </div>
   )
 }

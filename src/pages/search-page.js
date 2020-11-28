de8fd3a64b675656c7bd72c36be985f68446cb84
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-11-28 14:57:04 -0800

    created search bar using antd

--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -1,22 +1,63 @@
 import React from "react"
 import "./search-page.css"
-import { Typography, Input } from "antd"
+import { Typography, Input, Select, Button } from "antd"
 import "antd/dist/antd.css"
 
 const { Title } = Typography
+const { Option } = Select
+const { Search } = Input
 
 class SearchPage extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      searchType: "user",
+    }
+    this.handleSelectChange = this.handleSelectChange.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+
+  handleSelectChange(value) {
+    this.setState({ searchType: value })
+  }
+
+  handleSearch(searchQuery) {
+    // get search results here, store in state, use to create a list of user cards
+    if (searchQuery !== "") {
+      const { searchType } = this.state
+      // eslint-disable-next-line no-alert
+      alert(`Searching for ${searchQuery} in ${searchType}`)
+    }
   }
 
   render() {
+    const { searchType } = this.state
     return (
       <div className="background">
         <div className="page-body">
           <Title size={1}>Search</Title>
-          <Input className="search-bar" size="large" placeholder="Search for a ___" />
+          <div className="search-bar-container">
+            <Select
+              className="search-type-input"
+              defaultValue="user"
+              size="large"
+              onChange={this.handleSelectChange}
+            >
+              <Option value="user">user</Option>
+              <Option value="community">community</Option>
+            </Select>
+            <Search
+              className="search-bar"
+              size="large"
+              placeholder={`Search for a ${searchType}`}
+              enterButton={
+                <Button type="primary" className="search-bar-button" size="large">
+                  Search
+                </Button>
+              }
+              onSearch={this.handleSearch}
+            />
+          </div>
         </div>
       </div>
     )

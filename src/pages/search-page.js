a6a91de6a5bcdb70bc5b75ed6792083d0bf1fb10
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-11-28 13:45:06 -0800

    removed unused components for search page, converted search-page to class component

--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -1,18 +1,25 @@
 import React from "react"
 import "./search-page.css"
-import { Typography } from "antd"
+import { Typography, Input } from "antd"
 import "antd/dist/antd.css"
 
 const { Title } = Typography
 
-function SearchPage() {
-  return (
-    <div className="background">
-      <div className="page-body">
-        <Title size={1}>Search</Title>
-      </div>
-    </div>
-  )
-}
+class SearchPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
+  render() {
+    return (
+      <div className="background">
+        <div className="page-body">
+          <Title size={1}>Search</Title>
+          <Input className="search-bar" size="large" placeholder="Search for a ___" />
+        </div>
+      </div>
+    )
+  }
+}
 export default SearchPage

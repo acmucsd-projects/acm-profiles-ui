d019d3c60f60b4c8d1fbc4f37e68c3ddbb5ca0fe
abn <annie.nguyen.804@gmail.com>
2020-11-28 17:51:31 -0800

    General format of members list

--- src/components/MemberLists/Member.js
+++ src/components/MemberLists/Member.js
@@ -1,12 +1,40 @@
 import React from "react"
 import "./style.css"
+import PropTypes from "prop-types"
+import { Typography } from "antd"
 
-const Member = () => {
+const { Title, Text } = Typography
+
+const Member = (props) => {
+  const { firstName, lastName, major, graduationYear, college } = props
+
   return (
     <div className="member">
-      <div />
+      <div className="left-div">
+        <img className="member-profile-pic" src="./logo192.png" alt="member" />
+      </div>
+
+      <div className="middle-div">
+        <Title level={4}>
+          {firstName} {lastName}
+        </Title>
+      </div>
+
+      <div className="right-div">
+        <Text>{major}</Text>
+        <Text>{college} College</Text>
+        <Text>Class of {graduationYear}</Text>
+      </div>
     </div>
   )
+}
+
+Member.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  major: PropTypes.string.isRequired,
+  college: PropTypes.string.isRequired,
+  graduationYear: PropTypes.string.isRequired,
 }
 
 export default Member
--- src/components/MemberLists/MemberList.js
+++ src/components/MemberLists/MemberList.js
@@ -1,13 +1,41 @@
-import React from "react"
+import React, { Component } from "react"
+import Member from "./Member"
 import "./style.css"
 
 // container for a list of members, used in CommunityProfile and UserProfile
-const MemberList = () => {
-  return (
-    <div className="memberlist">
-      <div />
-    </div>
-  )
+class MemberList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      members: {
+        member1: ["John", "Doe", "Undecided", "2020", "Marshall"],
+      },
+    }
+  }
+
+  render() {
+    const { members } = this.state
+    const membersArr = []
+
+    Object.entries(members).forEach(([key, value]) => {
+      membersArr.push(
+        <Member
+          firstName={value[0]}
+          lastName={value[1]}
+          major={value[2]}
+          graduationYear={value[3]}
+          college={value[4]}
+        />
+      )
+    })
+
+    return (
+      <div className="member-list-container">
+        <div className="member-list-members-container">{membersArr}</div>
+      </div>
+    )
+  }
 }
 
 export default MemberList

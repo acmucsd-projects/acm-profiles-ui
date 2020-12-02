3250d5afdee600c91b0d570186435e30a6909f6a
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-12-01 17:37:03 -0800

    created logo on navbar, improved flexible sizing

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,23 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./style.css"
+import { Typography } from "antd"
+import ACMLogo from "../../images/acm-logo.png"
+
+const { Title } = Typography
 
 const NavigationBar = () => {
   return (
     <div className="navigationbar">
       <nav>
+        <div className="nav-logo-container">
+          <img src={ACMLogo} alt="acm logo" className="nav-logo-image" />
+          <div style={{ paddingLeft: "5px", paddingTop: "5px" }}>
+            <Title level={2} underline>
+              Profiles
+            </Title>
+          </div>
+        </div>
         <Link to="/" className="navigation-button">
           Log In Page
         </Link>

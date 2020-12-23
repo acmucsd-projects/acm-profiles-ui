bd35ad9854760e74ae34eb40c89be0d862224e3b
jettbui <jettcbui@gmail.com>
2020-12-23 15:17:57 -0800

    CommunityHeader: pass by props, add editability

--- src/components/CommunityProfile/CommunityHeader.js
+++ src/components/CommunityProfile/CommunityHeader.js
@@ -1,79 +1,93 @@
-import React, { Component } from "react"
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React from "react"
 import PropTypes from "prop-types"
-import { Typography, Button } from "antd"
+import { Typography, Button, Tooltip } from "antd"
 import "./CommunityHeader.css"
 
 const { Title, Text } = Typography
 
-class CommunityHeader extends Component {
-  // Required props: community (currently placeholder), editable, joinable
-  constructor(props) {
-    super(props)
-    this.state = {
-      /* vvv placeholder information vvv */
-      community: {
-        image:
-          "https://cravingsbychrissyteigen.com/wp-content/uploads/2020/05/CookingWithAlcohol_Square.jpg",
-        name: "ACM Cooking",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      },
-      /* ^^^ placeholder information ^^^ */
-      editable: props.editable,
-      joinable: props.joinable,
-    }
-  }
+function CommunityHeader(props) {
+  // Required props: community, canEdit, canJoin, editing, updateEditing
+  const { community, canEdit, canJoin, editing, updateEditing, setCommunity, setImageModal } = props
+  const buttons = []
 
-  render() {
-    const {
-      community: { image, name, description },
-      editable,
-      joinable,
-    } = this.state
-
-    const buttons = []
-
-    if (joinable)
-      buttons.push(
-        <Button className="community-button" type="primary" size="large">
-          + Join
-        </Button>
-      )
-    else
-      buttons.push(
-        <Button className="community-button" type="primary" size="large">
-          Leave
-        </Button>
-      )
-
-    if (editable)
-      buttons.push(
-        <Button className="community-button" size="large">
-          Edit Community
-        </Button>
-      )
-    return (
-      <div className="community-header-container">
-        <div className="community-image-container">
-          <img className="community-image" src={image} alt="community" />
-        </div>
-        <div className="community-info-container">
-          <div className="community-info-title-container">
-            <div className="community-info-name-container">
-              <Title level={2}>{name}</Title>
-            </div>
-            <div className="community-info-button-container">{buttons}</div>
-          </div>
-          <Text>{description}</Text>
-        </div>
-      </div>
+  if (canJoin)
+    buttons.push(
+      <Button key="join-community-key" className="community-button" type="primary" size="large">
+        + Join
+      </Button>
     )
+  else
+    buttons.push(
+      <Button key="leave-community-key" className="community-button" type="primary" size="large">
+        Leave
+      </Button>
+    )
+
+  if (canEdit)
+    buttons.push(
+      <Button className="community-button" size="large" onClick={() => updateEditing(true)}>
+        Edit Community
+      </Button>
+    )
+
+  const handleDescriptionChange = (newDescription) => {
+    const tempCommunity = {}
+    Object.assign(tempCommunity, community)
+    tempCommunity.description = newDescription
+    setCommunity(tempCommunity)
   }
+
+  return (
+    <div className="community-header-container">
+      <div className="community-image-container">
+        {editing && (
+          <span>
+            <Tooltip title="Click to edit">
+              <img
+                className="community-image"
+                src={community.image}
+                alt="community"
+                onClick={() => setImageModal(true)}
+              />
+            </Tooltip>
+          </span>
+        )}
+        {!editing && <img className="community-image" src={community.image} alt="community" />}
+      </div>
+      <div className="community-info-container">
+        <div className="community-info-title-container">
+          <div className="community-info-name-container">
+            <Title level={2}>{community.name}</Title>
+          </div>
+          <div className="community-info-button-container">{buttons}</div>
+        </div>
+        {editing && (
+          <Text
+            editable={{ autoSize: { minRows: 5, maxRows: 5 }, onChange: handleDescriptionChange }}
+          >
+            {community.description}
+          </Text>
+        )}
+        {!editing && <Text>{community.description}</Text>}
+      </div>
+    </div>
+  )
 }
 
 CommunityHeader.propTypes = {
-  joinable: PropTypes.bool.isRequired,
-  editable: PropTypes.bool.isRequired,
+  community: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+  canJoin: PropTypes.bool.isRequired,
+  canEdit: PropTypes.bool.isRequired,
+  editing: PropTypes.bool.isRequired,
+  updateEditing: PropTypes.func.isRequired,
+  setCommunity: PropTypes.func.isRequired,
+  setImageModal: PropTypes.func.isRequired,
 }
 
 export default CommunityHeader

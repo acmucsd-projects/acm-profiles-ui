ca44c35b5fa076581645544b9f4e557e9347c70a
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-12-27 15:12:38 -0800

    fixed linting errors

--- src/url-wrappers.js
+++ src/url-wrappers.js
@@ -112,36 +112,39 @@ export async function patchUserProfile(patchData) {
     })
 }
 export async function patchUserSocials(patchData) {
-  return axios({
-    method: "PATCH",
-    url: `${apiUrl}/user/profile/socials/${currentUUID}`,
-    data: patchData,
-  })
-    .then((data) => {
-      // console.log(data)
+  return (
+    axios({
+      method: "PATCH",
+      url: `${apiUrl}/user/profile/socials/${currentUUID}`,
+      data: patchData,
     })
-    .catch((error) => {
-      // error!
-      if (error.response) {
-        /*
-         * The request was made and the server responded with a
-         * status code that falls out of the range of 2xx
-         */
-        console.log(error.response.data)
-        console.log(error.response.status)
-        console.log(error.response.headers)
-      } else if (error.request) {
-        /*
-         * The request was made but no response was received, `error.request`
-         * is an instance of XMLHttpRequest in the browser and an instance
-         * of http.ClientRequest in Node.js
-         */
-        console.log(error.request)
-      } else {
-        // Something happened in setting up the request and triggered an Error
-        console.log("Error", error.message)
-      }
-    })
+      // eslint-disable-next-line no-unused-vars
+      .then((data) => {
+        // console.log(data)
+      })
+      .catch((error) => {
+        // error!
+        if (error.response) {
+          /*
+           * The request was made and the server responded with a
+           * status code that falls out of the range of 2xx
+           */
+          console.log(error.response.data)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+        } else if (error.request) {
+          /*
+           * The request was made but no response was received, `error.request`
+           * is an instance of XMLHttpRequest in the browser and an instance
+           * of http.ClientRequest in Node.js
+           */
+          console.log(error.request)
+        } else {
+          // Something happened in setting up the request and triggered an Error
+          console.log("Error", error.message)
+        }
+      })
+  )
 }
 export function logOut() {
   currentUUID = ""

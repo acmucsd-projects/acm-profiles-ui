ef3c8004e5147691d34e0cba2448035dc80ed81e
Anurag Asthana <anurag.a.asthana@gmail.com>
2020-12-28 17:49:48 -0800

    Adding Community Card Component

---
+++ src/components/CommunityList/CommunityCard.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { Typography } from "antd"
+import PropTypes from "prop-types"
+import "antd/dist/antd.css"
+import "./CommunityCard.css"
+
+const { Title, Text } = Typography
+function CommunityCard(props) {
+  // Required props: key, communityName, description, numMembers communityImageURL
+  const { cid, communityName, description, numMembers, communityImageURL } = props
+  return (
+    <Link to={`/user/${cid}`}>
+      <div className="community-card-container">
+        <div className="community-image-container">
+          <img className="community-card-profile-image" src={communityImageURL} alt="user" />
+        </div>
+        <div className="community-name-container">
+          <Title level={4}>{communityName}</Title>
+        </div>
+        <div className="community-details-container">
+          <Text>{description}</Text>
+          <Text>{numMembers}</Text>
+        </div>
+      </div>
+    </Link>
+  )
+}
+CommunityCard.propTypes = {
+  cid: PropTypes.string.isRequired,
+  communityName: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  numMembers: PropTypes.number.isRequired,
+  communityImageURL: PropTypes.string.isRequired,
+}
+
+export default CommunityCard
---
+++ src/components/CommunityList/CommunityList.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable react/no-unused-prop-types */
+import React from "react"
+import PropTypes from "prop-types"
+import CommunityCard from "./CommunityCard"
+import "./CommunityList.css"
+
+function CommunityList(props) {
+  // Required props: userList
+
+  const communityList = props.communityList
+  const communityCardList = communityList.map((community) => {
+    const description = community.description != null ? community.description : "Undecided"
+    const imagesrc =
+      community.profile_pic != null
+        ? community.profile_pic
+        : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
+    return (
+      <CommunityCard
+        cid={community.cid}
+        communityName={community.communityName}
+        description={description}
+        numMembers={community.numMembers}
+        communityImageURL={imagesrc}
+      />
+    )
+  })
+  return <div>{communityCardList}</div>
+}
+
+CommunityList.propTypes = {
+  communityList: PropTypes.instanceOf(Array).isRequired,
+}
+
+export default CommunityList
